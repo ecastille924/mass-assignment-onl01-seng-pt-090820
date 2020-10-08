a2fa1ffed924c264371548531d28a424514e8b45
Erick Castille <ecastille924@gmail.com>
2020-10-08 23:31:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize (attributes)
-    atrributes.each do |key, value| self.send(("#key=")), value
+    atrributes.each{|key, value| self.send(("#key="), value)}
   end
 end
